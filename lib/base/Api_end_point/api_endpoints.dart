855d2c15fd7a337309244234b7461a033f9292fb
Umesh <globaltechumesh11@gmail.com>
2025-01-24 11:23:37 +0545

    End Point

--- lib/base/Api_end_point/api_endpoints.dart
+++ lib/base/Api_end_point/api_endpoints.dart
@@ -4,13 +4,6 @@ class ApiEndpoint {
   static const String baseUrl = kDebugMode ? 'http://192.168.1.64:3000' : 'http://192.168.1.64:3000';
   static const String imagebaseUrl = kDebugMode ? 'http://192.168.101.2:8080' : 'http://192.168.101.2:8080';
 
-  static const String forgetPassword = '';
-  static const String signup = '/user/sign-up/';
-  static const String googlesign = '/credentials/access_token';
-  static const String signin = '/user/sign-in/';
-  static const String createUser = '/user/profile-setup/';
-  static const String getUserProfile = '/user/get-user-profile/';
-  static const String getProjectDetail = '/user/get-user-profile/';
   static const String getOrderReport = '/MasterList/PendingOrderVerifyList/';
   static const String createProduct = '/GeneralLedger/SaveProductRelated/';
   static const String createCustomer= '/users/create/';
