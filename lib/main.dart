a89d805dba790fa68dcdafb5b611ca1916c25924
Umesh <globaltechumesh11@gmail.com>
2025-01-28 12:21:25 +0545

    Product

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class GetTest extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
       initialBinding: AppBindings(),
-      initialRoute: Routes.getCustomer,
+      initialRoute: Routes.getProductPage,
       getPages: Routes.routes,
     );
   }
